899ee88f2d7c11afb4791f0532abc704296bc43b
Bidkar Aragón Cárdenas <bidkar.aragon@udo.mx>
2020-01-20 13:32:55 -0700

    Adding packages
    http example with get method.

--- 03_Proyectos/mi_primer_proyecto_dart/bin/main.dart
+++ 03_Proyectos/mi_primer_proyecto_dart/bin/main.dart
@@ -1,5 +1,15 @@
-import 'package:mi_primer_proyecto_dart/mi_primer_proyecto_dart.dart' as mi_primer_proyecto_dart;
+import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) {
-  print('Hello world: ${mi_primer_proyecto_dart.calculate()}!');
+// import 'package:mi_primer_proyecto_dart/mi_primer_proyecto_dart.dart' as mi_primer_proyecto_dart;
+
+void main(List<String> arguments) async {
+  // print('Hello world: ${mi_primer_proyecto_dart.calculate()}!');
+  var url = 'https://reqres.in/api/users/2';
+
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    print(response);
+  } else {
+    print('La petición a ${url} falló con el código de respuesta ${response.statusCode}');
+  }
 }
