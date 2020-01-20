962011003a226394ca60e29b233cc30c19016436
Bidkar Aragón Cárdenas <bidkar.aragon@udo.mx>
2020-01-20 12:54:48 -0700

    Primer proyecto Dart

---
+++ 03_Proyectos/mi_primer_proyecto_dart/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:mi_primer_proyecto_dart/mi_primer_proyecto_dart.dart' as mi_primer_proyecto_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mi_primer_proyecto_dart.calculate()}!');
+}
---
+++ 03_Proyectos/mi_primer_proyecto_dart/test/mi_primer_proyecto_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:mi_primer_proyecto_dart/mi_primer_proyecto_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
